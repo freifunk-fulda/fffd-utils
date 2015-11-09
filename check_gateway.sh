bd2f2f043051a7d0ec053bea399aa80215305660
Sven Reissmann <sven@0x80.io>
2015-11-09 23:17:31 +0100

    use the new chack gateway script

--- check_gateway.sh
+++ check_gateway.sh
@@ -1,44 +1,97 @@
 #!/bin/bash
 
 # check whether the gateway has access to the internet using the VPN-tunnel
-# interface. if so, turn on the BATMAN server mode and start services like
-# dhcp server, router advertisement daemon, and so on. otherwise, disable 
-# server mode and stop the services.
-
-source /etc/gateway_enabled
-
-INTERFACE="fffd.internet"
-BANDWIDTH=54MBit/54MBit
+# interface. if so, turn on the BATMAN server mode and start the dhcp and
+# radvd services.
+# if no internet connectivity is available using the vpn uplink, select
+# another vpn provider, restart openvpn and test connectivity again until
+# no vpn providers are left.
 
 shopt -s nullglob
 
-# Test whether gateway is connected to the outer world via VPN
-ping -q -I $INTERFACE 8.8.8.8 -c 2 -i 1 -W 5 >/dev/null 2>&1
+# vpn interface and advertised bandwidth
+#
+INTERFACE="fffd.internet"
+BANDWIDTH=54MBit/54MBit
 
-if [ ${GATEWAY_ENABLED} -eq 1 -a $? -eq 0 ]; then
-    NEW_STATE=server
-else
-    NEW_STATE=off
+
+# it is possible to manually enable or disable this gateway by using the 
+# parameter GATEWAY_ENABLED in /etc/gateway_enabled. if not set to one, 
+# the following block will disable the gateway mode and stop services.
+#
+source /etc/gateway_enabled
+
+if [ ${GATEWAY_ENABLED} -ne 1 ]; then
+	for MESH in /sys/class/net/*/mesh; do
+		if [ "$(cat $MESH/gw_mode)" != "off" ]; then
+			logger "check_gateway: manually disabling gateway mode"
+
+			echo $NEW_STATE > $MESH/gw_mode
+			echo $BANDWIDTH > $MESH/gw_bandwidth
+
+			systemctl stop isc-dhcp-server.service
+			systemctl stop radvd.service
+		fi
+	done
+
+	exit 0
 fi
 
-# Iterate through network interfaces in sys file system
+
+# get the currently used vpn provider, which we can detect by looking at
+# the symlink /etc/openvpn/openvpn.conf
+#
+CURRENT_PROVIDER=$(readlink /etc/openvpn/openvpn.conf)
+
+
+# get all available VPN providers, minus the one we are currently using
+#
+PROVIDERS=("$(ls -1 /etc/openvpn/pp-* |shuf |xargs -L1 basename |grep -v "^${CURRENT_PROVIDER}$")")
+
+
+# test internet connectivity via vpn. if we can not ping google dns, select
+# another vpn provider and try again.
+#
+NEW_STATE=off
+for p in $PROVIDERS; do
+	ping -q -I $INTERFACE 8.8.8.8 -c 2 -i 1 -W 5 &>/dev/null
+
+	if [ $? -eq 0 ]; then
+		NEW_STATE=server
+		break
+	else
+		logger "check_gateway: change vpn provider to $p"
+		ln -f -s /etc/openvpn/$p /etc/openvpn/openvpn.conf
+		systemctl restart openvpn@openvpn
+	fi
+
+	sleep 5
+done
+
+
+# iterate through network interfaces in sys file system. if new state is not
+# equal old state, change gateway mode accordingly
+#
 for MESH in /sys/class/net/*/mesh; do
     # Check whether gateway modus needs to be changed
     OLD_STATE="$(cat $MESH/gw_mode)"
     [ "$OLD_STATE" == "$NEW_STATE" ] && continue
     
+    logger "check_gateway: changing batman gateway mode to $NEW_STATE"
     echo $NEW_STATE > $MESH/gw_mode
     echo $BANDWIDTH > $MESH/gw_bandwidth
-    logger "check_gateway: batman gateway mode changed to $NEW_STATE"
 
     if [ "$NEW_STATE" == "server" ]; then
         # Restart DHCP server if gateway modus has been activated
+	logger "check_gateway: Restarting dhcp and radvd."
         systemctl restart isc-dhcp-server.service
         systemctl restart radvd.service
 
     else
         # Shutdown DHCP server to prevent renewal of leases
+	logger "check_gateway: Stopping dhcp and radvd."
         systemctl stop isc-dhcp-server.service
         systemctl stop radvd.service
     fi
 done
+
