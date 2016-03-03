ada5ba588c28b530897d2b40b6535431eeb3610d
Sven Reissmann <sven@0x80.io>
2016-03-03 22:56:29 +0100

    rework update-icvpn.sh script to support peering type ice.
    this is needed, because we still want this script to create ROA and DNS
    delegation files for us, even if peering type is ice.

--- update-icvpn.sh
+++ update-icvpn.sh
@@ -1,13 +1,23 @@
 #!/bin/bash
 #
-# This script will update ICVPN peerings and DNS delegations.
+# Depending on cli parameter $1, this script will
+#  1) update tinc and bird configuration for ICVPN peerings
+#  2) updates DNS delegations to other freifunk communities
+#     and generate a ROA table
+#
 # It needs to run by cron.
+# Parameter $1 may be "icvpn" or "ice"
 #
 
 [ "$(whoami)" != 'root' ] && (
 	logger "update-icvpn: error: please execute as user root"
 	exit 1
 )
+
+# Get mode (see decription at top; ice == 2, icvpn == 1 & 2)
+MODE=ice
+[ $# -eq 1 ] && MODE=$1
+
 
 TINC_NETWORK=icvpn
 
@@ -22,6 +32,7 @@ BIRD6_PEERS=$BIRD_ROOT/icvpn/bird6-peers-icvpn.conf
 
 BIND_ROOT=/etc/bind
 BIND_CONFIG=$BIND_ROOT/named.conf.icvpn
+
 
 update_roa() (
 	$ICVPN_SCRIPTS/mkroa -4 -m 24 -f bird -x fulda -s $ICVPN_META > $BIRD4_ROA
@@ -49,26 +60,36 @@ update_bind() (
 
 PATH=/usr/sbin:$PATH
 
-# tinc vpn
-cd /etc/tinc/$TINC_NETWORK/
-git remote update >/dev/null
-
-if [ $FORCE_VPN ] || [ $(git rev-parse HEAD) != $(git rev-parse @{u}) ]; then
-	logger "update-icvpn: repo icvpn: update available"
-	git pull origin master
-	# post-merge hook handles configuration update
+# depending on $MODE, update tinc peers
+#
+if [ "$MODE" == "icvpn" ]; then
+	cd /etc/tinc/$TINC_NETWORK/
+	git remote update >/dev/null
+	
+	if [ $FORCE_VPN ] || [ $(git rev-parse HEAD) != $(git rev-parse @{u}) ]; then
+		logger "update-icvpn: repo icvpn: update available"
+		git pull origin master
+		# post-merge hook handles configuration update
+	fi
 fi
 
-# icvpn-meta
+# depending on $MODE, update bird peers, ROA and DNS delegations
+#
 cd $ICVPN_META
 git remote update >/dev/null
 
 if [ $FORCE_META ] || [ $(git rev-parse HEAD) != $(git rev-parse @{u}) ]; then
 	logger "update-icvpn: repo icvpn-meta: update available"
 	git pull origin master
+
+	# update bird peers
+	if [ "$MODE" == "icvpn" ]; then
+		update_bgp_peers
+		update_bgp6_peers
+	fi
+
+	# update ROA and DNS delegations
 	update_roa
-	update_bgp_peers
-	update_bgp6_peers
 	update_bind
 fi
 
